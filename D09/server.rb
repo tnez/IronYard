81cfa3902adb4d0dadc1419534518e3703e95355
Travis Nesland <tnesland@gmail.com>
2015-01-15 13:08:51 -0500

    User can create new post.

--- D09/server.rb
+++ D09/server.rb
@@ -1,11 +1,28 @@
 require 'sinatra'
 require 'data_mapper'
+require './models/post.rb'
 
 DataMapper.setup(:default,
                  'mysql://root@localhost/blog')
 
 DataMapper.finalize.auto_upgrade!
 
+# Show
 get '/' do
   erb :index
+end
+
+# New
+get '/create_post' do
+  erb :create_post_form
+end
+
+# Create
+post '/create_post' do
+  p params
+  @post = Post.new
+  @post.title = params[:title]
+  @post.body = params[:body]
+  @post.save
+  redirect to '/'
 end
