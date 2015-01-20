f01617dc35dd7b68cac1ab0b235897876f729cb3
Travis Nesland <tnesland@gmail.com>
2015-01-20 14:50:36 -0500

    User can add new snippet.

--- D11/app/controllers/snippets_controller.rb
+++ D11/app/controllers/snippets_controller.rb
@@ -4,5 +4,18 @@ class SnippetsController < ApplicationController
     @snippets = Snippet.all
     @char_limit = 75
   end
+
+  def new
+    @snippet = Snippet.new
+  end
+
+  def create
+    @snippet = Snippet.create snippet_params
+    redirect_to snippets_path
+  end
+
+  def snippet_params
+    params.require(:snippet).permit(:title,:desc,:code)
+  end
   
 end
